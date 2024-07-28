9ebd8b0c0eff69edb8e67e93d576671ab8ace33f
Qooniee <kawai@mba2.local>
2024-07-28 23:12:13 +0900

    simple pages

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,55 @@
+/**
+ * 全ページ共通のコンポーネントを定義
+ * page.tsxと同じ階層にlayout.tsxを配置すると
+ * その階層以下のディレクトリで共通となる
+ * 全ページ共通のナビーゲーションやサイドバーを記載
+ */
+
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Navigation from "@/components/auth/Navigation"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "QoonieeBlog",
+  description: "QoonieeBlog",
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+const RootLayout = async ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col">
+          <Navigation />
+
+          <main className="container mx-auto max-w-screen-md flex-1 px-2">
+            {children}
+          </main>
+
+          {/* フッター */}
+          <footer className="py-5">
+            <div className="text-center text-sm">
+              Copyright © All rights reserved |{" "}
+              <a
+                href="https://github.com/Qooniee/auth-blog"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+              >
+                Qooniee
+              </a>
+            </div>
+          </footer>
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+//App Router Root File
+// メインページ
+const Home = () => {
+  return (
+    <div>
+      <div>Main</div>
+    </div>
+  )
+}
+
+export default Home
